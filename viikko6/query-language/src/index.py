fe53a0c1688934e981d9c2359e10ca7d74f47d62
eerosu <eero.suokko@helsinki.fi>
2025-12-08 21:46:47 +0200

    Tehtävä 3

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players.txt"
@@ -16,6 +16,25 @@ def main():
     for player in stats.matches(matcher):
         print(player)
 
+    matcher = And(
+        Not(HasAtLeast(2, "goals")),
+        PlaysIn("NYR")
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
+
+    matcher = And(
+        HasFewerThan(2, "goals"),
+        PlaysIn("NYR")
+    )
+    for player in stats.matches(matcher):
+        print(player)
+
+    filtered_with_all = stats.matches(All())
+    print(len(filtered_with_all))
+
 
 if __name__ == "__main__":
     main()
+
